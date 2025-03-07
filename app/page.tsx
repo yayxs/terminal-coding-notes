6aff4891ef3c3e7d0ade28c2e1f6977f66d16b82
yayxs <leaderywl@163.com>
2025-03-07 15:26:19 +0800

    chore: init next app

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+
+export default function Home() {
+  return (
+    <div>
+      <button className="btn">Button</button>
+      <div className="stack size-28">
+        <div className="border-base-content card bg-base-100 border text-center">
+          <div className="card-body">A</div>
+        </div>
+        <div className="border-base-content card bg-base-100 border text-center">
+          <div className="card-body">B</div>
+        </div>
+        <div className="border-base-content card bg-base-100 border text-center">
+          <div className="card-body">C</div>
+        </div>
+      </div>
+    </div>
+  );
+}
