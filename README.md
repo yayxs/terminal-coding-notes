4618ad430f59b995b6827199c6501b2fca08128e
yayxs <leaderywl@163.com>
2025-03-07 13:37:28 +0800

    chore: init style collection

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-templates-generator
-Vue CLI 项目模板自动生成器
--- utils/index.js
+++
@@ -1,9 +0,0 @@
-exports.isNotEmpty = (name) => {
-  return (v) => {
-    if (!v || v.trim === "") {
-      return `${name}为必填项`;
-    } else {
-      return true;
-    }
-  };
-};
