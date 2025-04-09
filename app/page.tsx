2736a1da11def6637517b1a7ad826d610193b271
yayxs <leaderywl@163.com>
2025-04-09 11:12:44 +0800

    chore: update project configuration and dependencies, remove unused VSCode settings, and enhance global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,8 @@
-
+import { Globe } from "@/components/magicui/globe";
 export default function Home() {
   return (
     <div>
-      <button className="btn">Button</button>
-      <div className="stack size-28">
-        <div className="border-base-content card bg-base-100 border text-center">
-          <div className="card-body">A</div>
-        </div>
-        <div className="border-base-content card bg-base-100 border text-center">
-          <div className="card-body">B</div>
-        </div>
-        <div className="border-base-content card bg-base-100 border text-center">
-          <div className="card-body">C</div>
-        </div>
-      </div>
+      <Globe />
     </div>
   );
 }
